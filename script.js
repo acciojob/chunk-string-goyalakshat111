08b85eb25b67645fff6854d8406a8b7ac743cc7a
Akshat Goyal <goyalakshat111@gmail.com>
2024-12-28 16:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function stringChop(str, size) {
   // your code here
+	if(str==null){
+		return []
+	}
 	let arr = []
 	for(let i=0;i<str.length;i=i+size){
 		 arr.push(str.slice(i,i+size))
@@ -10,6 +13,6 @@ function stringChop(str, size) {
 
 // Do not change the code below
 
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
